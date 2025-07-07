04243ebb578e39ee729111ef388b6a247cdde112
Simon Hartcher <simon@simonhartcher.com>
2025-07-07 13:44:55 +1000

    feat: jumping is working

--- src/Player.zig
+++ src/Player.zig
@@ -1,57 +1,57 @@
 const Self = @This();
 
-const default_movement_speed: f32 = 250.0;
-const default_jump_strength: f32 = 7.0;
-const default_gravity: f32 = 0;
+coin_collected: Signal,
 
-base: CharacterBody3D,
-view: Node3D,
-movement_speed: f32,
-jump_strength: f32,
-movement_velocity: Vector3,
-rotation_direction: f64,
-gravity: f32,
-previously_floored: bool,
-jump_single: bool,
-jump_double: bool,
-coins: u32,
+base: *CharacterBody3D,
+movement_speed: f64 = 250.0,
+jump_strength: f64 = 7.0,
+movement_velocity: Vector3 = .zero,
+rotation_direction: f64 = 0,
+gravity: f64 = 0,
+previously_floored: bool = false,
+jump_single: bool = true,
+jump_double: bool = true,
+coins: u32 = 0,
 
-particles_trail: CPUParticles3D,
-sound_footsteps: AudioStreamPlayer,
-model: Node3D,
-animation: AnimationPlayer,
+view: *Node3D = undefined,
+particles_trail: *CPUParticles3D = undefined,
+sound_footsteps: *AudioStreamPlayer = undefined,
+model: *Node3D = undefined,
+animation: *AnimationPlayer = undefined,
+
+pub fn init(base: *CharacterBody3D) Self {
+    return .{
+        .base = base,
+        .coin_collected = .init(),
+    };
+}
 
 pub fn _ready(self: *Self) void {
     if (Engine.isEditorHint()) return;
 
-    // init variables
-    self.movement_speed = default_movement_speed;
-    self.jump_strength = default_jump_strength;
-    self.gravity = default_gravity;
-    self.previously_floored = false;
-    self.jump_single = true;
-    self.jump_double = true;
-    self.coins = 0;
-
     // onready
     self.particles_trail = CPUParticles3D.downcast(
-        self.base.getNode(.fromString(String.fromLatin1("./ParticlesTrail"))).?,
+        self.base.getNode(.fromString(.fromLatin1("./ParticlesTrail"))).?,
     ) catch std.debug.panic("Failed to find ParticlesTrail", .{});
 
     self.sound_footsteps = AudioStreamPlayer.downcast(
-        self.base.getNode(.fromString(String.fromLatin1("./SoundFootsteps"))).?,
+        self.base.getNode(.fromString(.fromLatin1("./SoundFootsteps"))).?,
     ) catch std.debug.panic("Failed to find SoundFootsteps", .{});
 
     self.model = Node3D.downcast(
-        self.base.getNode(.fromString(String.fromLatin1("./Character"))).?,
+        self.base.getNode(.fromString(.fromLatin1("./Character"))).?,
     ) catch std.debug.panic("Failed to find Character", .{});
 
     self.animation = AnimationPlayer.downcast(
-        self.base.getNode(.fromString(String.fromLatin1("./Character/AnimationPlayer"))).?,
+        self.base.getNode(.fromString(.fromLatin1("./Character/AnimationPlayer"))).?,
     ) catch std.debug.panic("Failed to find AnimationPlayer", .{});
+
+    self.view = Node3D.downcast(
+        self.base.getNode(.fromString(.fromLatin1("../View"))).?,
+    ) catch std.debug.panic("Failed to find View", .{});
 }
 
-pub fn _physicsProcess(self: *Self, delta: f32) void {
+pub fn _physicsProcess(self: *Self, delta: f64) void {
     if (Engine.isEditorHint()) return;
 
     // handle functions
@@ -62,9 +62,9 @@ pub fn _physicsProcess(self: *Self, delta: f32) void {
     // movement
     var applied_velocity: Vector3 = self.base.getVelocity()
         .lerp(self.movement_velocity, delta * 10);
-    applied_velocity.y -= self.gravity;
-
+    applied_velocity.y = @floatCast(-self.gravity);
     self.base.setVelocity(applied_velocity);
+
     _ = self.base.moveAndSlide();
 
     // rotation
@@ -76,6 +76,7 @@ pub fn _physicsProcess(self: *Self, delta: f32) void {
 
     var rotation = self.base.getRotation();
     rotation.y = @floatCast(math.lerpAngle(@floatCast(rotation.y), self.rotation_direction, delta * 10));
+    self.base.setRotation(rotation);
 
     // falling/respawning
     if (self.base.getPosition().y < -10) {
@@ -97,19 +98,30 @@ pub fn _physicsProcess(self: *Self, delta: f32) void {
     self.previously_floored = self.base.isOnFloor();
 }
 
-fn handleControls(self: *Self, delta: f32) void {
-    var input: Vector3 = .init();
+fn handleControls(self: *Self, delta: f64) void {
+    var input: Vector3 = .zero;
     input.x = Input.getAxis(.fromLatin1("move_left"), .fromLatin1("move_right"));
     input.z = Input.getAxis(.fromLatin1("move_forward"), .fromLatin1("move_back"));
 
+    std.debug.print("{d} {d} {} {}\n", .{
+        Input.getActionStrength(.fromLatin1("move_left"), .{}),
+        Input.getActionStrength(.fromLatin1("move_right"), .{}),
+        Input.isActionPressed(.fromLatin1("move_left"), .{}),
+        Input.isActionPressed(.fromLatin1("move_right"), .{}),
+    });
+
+    if (!input.isZeroApprox()) {
+        std.debug.print("Input is not zero: {}\n", .{input});
+    }
+
     const rotation = self.view.getRotation();
-    input = input.rotated(Vector3.initXYZ(0, 1, 0), rotation.y);
+    input = input.rotated(.up, rotation.y);
 
     if (input.length() > 1) {
         input = input.normalized();
     }
 
-    self.movement_velocity = input.mulFloat(self.movement_speed).mulFloat(delta);
+    self.movement_velocity = input.mulFloat(self.movement_speed * delta);
 
     // jumping
     if (Input.isActionJustPressed(.fromLatin1("jump"), .{})) {
@@ -120,18 +132,73 @@ fn handleControls(self: *Self, delta: f32) void {
 }
 
 fn jump(self: *Self) void {
-    _ = self; // autofix
-    // TODO
+    // Audio.play("res://sounds/jump.ogg")
+
+    self.gravity = -self.jump_strength;
+    self.model.setScale(.initXYZ(0.5, 1.5, 0.5));
+
+    if (self.jump_single) {
+        self.jump_single = false;
+        self.jump_double = true;
+    } else {
+        self.jump_double = false;
+    }
 }
 
-fn handleGravity(self: *Self, delta: f32) void {
-    _ = self; // autofix
-    _ = delta; // autofix
+fn handleGravity(self: *Self, delta: f64) void {
+    self.gravity += (25 * @as(f64, @floatCast(delta)));
+
+    if (self.gravity > 0 and self.base.isOnFloor()) {
+        self.jump_single = true;
+        self.gravity = 0;
+    }
 }
 
-fn handleEffects(self: *Self, delta: f32) void {
-    _ = self; // autofix
-    _ = delta; // autofix
+fn handleEffects(self: *Self, delta: f64) void {
+    self.particles_trail.setEmitting(false);
+    self.sound_footsteps.setStreamPaused(true);
+
+    const current_animation = self.animation.getCurrentAnimation();
+
+    if (self.base.isOnFloor()) {
+        const velocity = self.base.getVelocity();
+        var horizontal_velocity: Vector2 = .initXY(velocity.x, velocity.z);
+
+        const speed_factor = horizontal_velocity.length() / self.movement_speed / delta;
+
+        if (speed_factor > 0.05) {
+            if (!current_animation.eql(.fromLatin1("walk"))) {
+                self.animation.play(.{
+                    .name = .fromLatin1("walk"),
+                    .custom_speed = 0.1,
+                });
+            }
+
+            if (speed_factor > 0.3) {
+                self.sound_footsteps.setStreamPaused(false);
+                self.sound_footsteps.setPitchScale(@floatCast(speed_factor));
+            }
+
+            if (speed_factor > 0.75) {
+                self.particles_trail.setEmitting(true);
+            }
+        } else if (!current_animation.eql(.fromLatin1("idle"))) {
+            self.animation.play(.{
+                .name = .fromLatin1("idle"),
+                .custom_speed = 0.1,
+            });
+        }
+    } else if (!current_animation.eql(.fromLatin1("jump"))) {
+        self.animation.play(.{
+            .name = .fromLatin1("jump"),
+            .custom_speed = 0.1,
+        });
+    }
+}
+
+pub fn collectCoin(self: *Self) void {
+    self.coins += 1;
+    self.coin_collected.emit(self.coins);
 }
 
 const Input = godot.class.Input;
@@ -140,6 +207,7 @@ const String = godot.builtin.String;
 const StringName = godot.builtin.StringName;
 const Vector2 = godot.builtin.Vector2;
 const Vector3 = godot.builtin.Vector3;
+const Signal = godot.builtin.Signal;
 const Node3D = godot.class.Node3D;
 const NodePath = godot.builtin.NodePath;
 const CPUParticles3D = godot.class.CPUParticles3D;
